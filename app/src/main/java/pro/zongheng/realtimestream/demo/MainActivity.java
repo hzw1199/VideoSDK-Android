8aee96fb258744b8debe27c985d4268557e8913c
wuzh <contact@zongheng.pro>
2023-04-17 18:18:26 +0800

    initial commit

---
+++ app/src/main/java/pro/zongheng/realtimestream/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package pro.zongheng.realtimestream.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
